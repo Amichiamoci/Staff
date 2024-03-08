3bc0e795c868cfda3a012ff733d33e7ea47de819
Richie314 <richieinleghorn@gmail.com>
2024-03-08 15:22:07 +0100

    Brackets fixed

--- load/config.php
+++ load/config.php
@@ -13,20 +13,20 @@ $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->ifPresent(array("DB_HOST", "DB_USER", "DB_NAME"))->notEmpty();
 $dotenv->safeLoad();
 if (isset($_ENV["DB_HOST"]))
-    define("MYSQL_HOST", $_ENV("DB_HOST"));
+    define("MYSQL_HOST", $_ENV["DB_HOST"]);
 if (isset($_ENV["DB_USER"]))
-    define("MYSQL_USER", $_ENV("DB_USER"));
+    define("MYSQL_USER", $_ENV["DB_USER"]);
 if (isset($_ENV["DB_PASSWORD"]))
-    define("MYSQL_PASSWORD", $_ENV("DB_PASSWORD"));
+    define("MYSQL_PASSWORD", $_ENV["DB_PASSWORD"]);
 if (isset($_ENV["DB_NAME"]))
-    define("MYSQL_USER", $_ENV("DB_NAME"));
+    define("MYSQL_USER", $_ENV["DB_NAME"]);
 
 if (isset($_ENV["WEBSITE_DOMAIN"]))
-    define("DOMAIN", $_ENV("WEBSITE_DOMAIN"));
+    define("DOMAIN", $_ENV["WEBSITE_DOMAIN"]);
 if (isset($_ENV["EMAIL_SOURCE"]))
-    define("EMAIL_SOURCE", $_ENV("EMAIL_SOURCE"));
+    define("EMAIL_SOURCE", $_ENV["EMAIL_SOURCE"]);
 if (isset($_ENV["CONTACT_EMAIL"]))
-    define("CONTACT_EMAIL", $_ENV("CONTACT_EMAIL"));
+    define("CONTACT_EMAIL", $_ENV["CONTACT_EMAIL"]);
 unset($dotenv);
 
 error_reporting(E_ALL);
