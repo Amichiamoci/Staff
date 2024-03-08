a38c5bf55613d719bbc02d3f9adc4a65c9516526
Richie314 <richieinleghorn@gmail.com>
2024-03-08 12:29:51 +0100

    Sql fixes

---
+++ queries/views/user.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE VIEW `users_extended` AS
+SELECT 
+    u.`id`, 
+    u.`user_name`, 
+    u.`is_admin`, 
+    u.`is_blocked`, 
+    MAX(s.`time_log`) AS "last_seen_time",
+    PrintOnlineTime(MAX(s.`time_log`)) AS "last_seen"
+FROM `utenti` AS u
+    LEFT OUTER JOIN `sessioni` s ON s.`user_id` = u.`id`
+GROUP BY u.`id`
+ORDER BY last_seen_time DESC, u.`user_name` DESC;
