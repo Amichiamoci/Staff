b360a1cc5de311c18cad0c9cfe216079ffb05b15
Richie314 <richieinleghorn@gmail.com>
2024-03-08 12:14:58 +0100

    Cookie handling

--- staff/iscrizioni/iscrivi.php
+++ staff/iscrizioni/iscrivi.php
@@ -5,24 +5,27 @@
     if (isset($_GET["iscrizione"]))
     {
         $cod_iscrizione = (int)$_GET["iscrizione"];
-    }
-    if (isset($_POST["cod_iscrizione"]))
+    } elseif (isset($_POST["cod_iscrizione"]))
     {
         $cod_iscrizione = (int)$_POST["cod_iscrizione"];
     }
 
-    //Persona di iscrivere
+    //Persona da iscrivere
     if (isset($_GET["id"]))
     {
         $id_anagrafica = (int)$_GET["id"];
-    }
-    if (isset($_POST["id"]))
+    } elseif (isset($_POST["id"]))
     {
         $id_anagrafica = (int)$_POST["id"];
+    } elseif (Cookie::Exists("id_anagrafica"))
+    {
+        $id_anagrafica = (int)Cookie::Get("id_anagrafica");
     }
+
     if (!isset($id_anagrafica) && $cod_iscrizione === 0)
     {
-        header("Location: index.php");
+        // Bad request
+        header("Location: ./index.php");
         exit;
     }
     
@@ -34,28 +37,23 @@
         exit;
     }
     $dati_staff = Staff::Get($connection, User::$Current->staff_id);
-    if ((!$dati_staff->is_subscribed() || !$dati_staff->is_referente) && !User::$Current->is_admin)
+    if (!$dati_staff->is_subscribed() && !User::$Current->is_admin)
     {
         header("Location: ../index.php");
         exit;
     }
     $edizione = Edizione::Current($connection);
-    function addDot($str)
-    {
-        return ".$str";
-    }
-    $allowed_ext = array(
-        "jpg",        "jpeg",       "gif",
-        "png",        "bmp",        "avif",
-        "tif",        "tiff",       "webp",
-        "heic",       "heif",       "pdf",        
-        "doc",        "docx",       "ppt",
-        "pptx");
-    $file_accept = join(", ", array_map("addDot", $allowed_ext));
     $errore = "";
     if ($cod_iscrizione !== 0)
     {
         $iscrizione = Iscrizione::Load($connection, $cod_iscrizione);
+        if (!isset($iscrizione))
+        {
+            header("Location: ./index.php");
+            exit;
+        }
+    } else {
+        $dati_anagrafici = Anagrafica::Load($connection, $id_anagrafica);
     }
     function form()
     {
@@ -64,7 +62,7 @@
         global $edizione;
         global $cod_iscrizione;
         global $iscrizione;
-        global $allowed_ext;
+        global $dati_anagrafici;
 
         if (Iscrizione::Exists($connection, $id_anagrafica, $edizione->id) && $cod_iscrizione === 0)
         {
@@ -72,9 +70,10 @@
             return;
         }
         
-        $nome_file = "upload/certificati/" . $edizione->year . 
-                "_certificato_" . 
-                str_replace(array(" ", ".", ",", ":", ";", "<", ">", "?", "'", "\"", "\\"), "_", $iscrizione->nome);
+        $nome_file = 
+            "/certificati/$edizione->year" . 
+            "_certificato_" . 
+            $dati_anagrafici->nome . " " . $dati_anagrafici->cognome;
         //Upload file certificato
         if (isset($_FILES["certificato"]) && !empty($_FILES["certificato"]) &&
             is_uploaded_file($_FILES["certificato"]['tmp_name']))
@@ -133,9 +132,9 @@
             $nome_iscrizione = htmlspecialchars($iscrizione->nome);
             if (strlen($nome_file) > 0)
             {
-                Cookie::Set("esit", "$nome_iscrizione iscritto correttamente ad Amichiamoci " . $edizione->year . ".", 10);
+                Cookie::Set("esit", "$nome_iscrizione iscritto correttamente ad Amichiamoci $edizione->year", 10);
             } else {
-                Cookie::Set("esit", "$nome_iscrizione iscritto SENZA CERTIFICATO ad Amichiamoci " . $edizione->year . ".", 10);
+                Cookie::Set("esit", "$nome_iscrizione iscritto SENZA CERTIFICATO ad Amichiamoci $edizione->year", 10);
             }
             header("Location: index.php");
             exit;
@@ -174,19 +173,23 @@
 
 <div class="container">
 
+<?php include "../../parts/edition.php";?>
+
 <!-- Form ----------------------------------------------------------------- -->
 
-<section id="send-message" class="send-message full-h flex center">
+<section class="full-h flex center">
     <div class="grid">
         <div class="column col-100">
             <form action="iscrivi.php" method="post" class="login-form" enctype="multipart/form-data">
                 <?php if ($cod_iscrizione === 0) { ?>
                     <h3>
-                        Stai iscrivendo <?= htmlspecialchars($iscrizione->nome) ?> ad Amichiamoci <?= $edizione->year ?>
+                        Stai iscrivendo 
+                        <?= htmlspecialchars("$dati_anagrafici->nome $dati_anagrafici->cognome") ?> 
+                        ad Amichiamoci <?= $edizione->year ?>
                     </h3>
                     <p class="text">
                         Se vuoi iscrivere un'altra persona (la cui anagrafica &egrave; da creare)
-                        <a class="link" href="./crea-anagrafica.php?success=iscrivi"> clicca qui </a><br>
+                        <a class="link" href="./crea-anagrafica.php?success=./iscrivi.php"> clicca qui </a><br>
                         Se vuoi iscrivere un'altra persona, la cui anagrafica &egrave; gi&agrave; nel DB, 
                         <a class="link" href="./index.php">clicca qui</a>
                     </p>
@@ -232,7 +235,7 @@
                             }
                             foreach ($lista_parrocchie as $parr)
                             {
-                                $label = acc($parr->nome);
+                                $label = htmlspecialchars($parr->nome);
                                 $id = $parr->id;
                                 if ($id === $predefined_parrocchia)
                                 {
@@ -253,7 +256,7 @@
                             });
                             foreach ($anagrafiche as $a)
                             {
-                                $label = acc($a->nome . " " . $a->cognome);
+                                $label = htmlspecialchars($a->nome . " " . $a->cognome);
                                 $id = $a->id;
                                 if ($id === $iscrizione->id_tutore)
                                 {
@@ -265,7 +268,7 @@
                         ?>
                     </select>
                     <label for="certificato">Certificato</label>
-                    <input type="file" name="certificato" id="certificato" accept="<?=$file_accept?>" />
+                    <input type="file" name="certificato" id="certificato" accept="<?= join(", ", ALLOWED_EXT_DOTS) ?>" />
                 </div>
                 <input class="button rounded" type="submit" name="iscrivi_submit" value="Conferma">
             </form>
