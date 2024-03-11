d0ec18b0bf1e23ad2129d8396e4765f81c5a788f
Richie314 <richieinleghorn@gmail.com>
2024-03-11 15:28:53 +0100

    Missing part

--- queries/views/tornei_e_partite.sql
+++ queries/views/tornei_e_partite.sql
@@ -5,8 +5,9 @@ SELECT
     e.anno,
     t.sport AS "codice_sport",
     sp.nome AS "sport",
+    sp.area,
     tipi_torneo.nome AS "tipo",
-    tipi_torneo.id AS "id_tipo"
+    tipi_torneo.id AS "tipo_id"
 FROM tornei t
     INNER JOIN edizioni e ON e.id = t.edizione
     INNER JOIN sport sp ON sp.id = t.sport
