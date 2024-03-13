26d9c2a5fbed2ee2dc12f4effeb146db208a9574
Richie314 <richieinleghorn@gmail.com>
2024-03-14 00:19:50 +0100

    Create file if missing

--- cron/check-file.php
+++ cron/check-file.php
@@ -9,15 +9,19 @@ if (!$connection)
 $curr_date = date("d-m-Y");
 $do_op = true;
 
-$myfile = fopen($file_name, "r") or die("Unable to open file!");
-if ($data = fread($myfile, 10))
+$myfile = fopen($file_name, "r");
+if ($myfile)
 {
-    $file_date = date('d-m-Y', strtotime($data));
-    echo "Lettura precedente: $file_date<br>\n";
-    echo "Data corrente: $curr_date<br>\n";
-    $do_op = date_diff(date_create($curr_date), date_create($file_date))->days >= 1;
+    if ($data = fread($myfile, 10))
+    {
+        $file_date = date('d-m-Y', strtotime($data));
+        echo "Lettura precedente: $file_date<br>\n";
+        echo "Data corrente: $curr_date<br>\n";
+        $do_op = date_diff(date_create($curr_date), date_create($file_date))->days >= 1;
+    }
+    fclose($myfile);
 }
-fclose($myfile);
+
 if ($do_op)
 {
     $myfile = fopen($file_name, "w") or die("Unable to open file!");
