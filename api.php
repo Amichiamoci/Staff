8558fd6b771e5ea7ef7b604d267218927d7bdd3f
Richie314 <richieinleghorn@gmail.com>
2024-08-27 18:12:43 +0200

    Typo

--- api.php
+++ api.php
@@ -292,7 +292,7 @@ switch ($resource)
                     'SportId' => (int)$r['codice_sport'],
 
                     'Church' => $r['nome_parrocchia_ospite'],
-                    'ChurchId' => (int)$r['id_parrocchia_ospite'],
+                    'ChurchId' => (int)$r['id_parrocchia_ospiti'],
                 ],
                 'GuestTeamScore' => null
             ];
