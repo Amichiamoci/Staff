4590a0ad0b368f6afe77c3a2eb32e3ca95cbcaeb
Richie314 <richieinleghorn@gmail.com>
2024-09-09 20:18:00 +0200

    Punteggio in tornei ad eliminazione diretta

--- queries/functions/partite.sql
+++ queries/functions/partite.sql
@@ -24,6 +24,7 @@ READS SQL DATA
 BEGIN
     DECLARE risultato INT DEFAULT NULL;
     DECLARE tavolino INT DEFAULT NULL;
+    DECLARE area VARCHAR(16);
 
     IF id IS NULL OR team IS NULL THEN
         RETURN 0;
@@ -48,11 +49,27 @@ BEGIN
         RETURN 0; -- La partita non esiste
     END IF;
 
+    SELECT TRIM(UPPER(`sport`.`area`)) INTO area
+    FROM `sport`
+        INNER JOIN `tornei` ON `tornei`.`sport` = `sport`.`id`
+        INNER JOIN `partite` ON `partite`.`torneo` = `tornei`.`id`
+    WHERE `partite`.`id` = id;
+
     IF EXISTS(
         SELECT `partite`.* 
         FROM `partite` 
         WHERE `partite`.`id` = id AND `partite`.`squadra_casa` = team) THEN
+
         -- Stiamo controllando la squadra di casa
+        
+        IF area = 'PALLAVOLO' THEN
+            RETURN CASE 
+                WHEN risultato > 1 THEN 3 -- 2 (o più) a 0
+                WHEN risultato = 1 THEN 2 -- 2 a 1
+                WHEN risultato = -1 THEN 1 -- 1 a 2
+                ELSE 0 END;
+        END IF;
+
         RETURN CASE 
             WHEN risultato > 0 THEN 3
             WHEN risultato = 0 THEN 1
@@ -60,6 +77,15 @@ BEGIN
     END IF;  
 
     -- Stiamo controllando la squadra ospite
+
+    IF area = 'PALLAVOLO' THEN
+        RETURN CASE 
+            WHEN risultato < -1 THEN 3 -- 0 a 2 (o più)
+            WHEN risultato = -1 THEN 2 -- 1 a 2
+            WHEN risultato = 1 THEN 1 -- 2 a 1
+            ELSE 0 END;
+    END IF;
+
     RETURN CASE 
         WHEN risultato < 0 THEN 3
         WHEN risultato = 0 THEN 1
