543de5e5a76d416f903e1f997f296b6f50c159f0
Richie314 <richieinleghorn@gmail.com>
2025-03-10 00:30:02 +0100

    Minor bug fix

--- queries/views/staff.sql
+++ queries/views/staff.sql
@@ -46,3 +46,26 @@ CREATE OR REPLACE VIEW `staff_attuali` AS
 SELECT s.*
 FROM `staff_per_edizione` s
 WHERE s.`anno` = YEAR(CURRENT_DATE);
+
+CREATE OR REPLACE VIEW `staff_correnti_incompleto` AS
+SELECT 
+    a.*, 
+    `parrocchie`.`nome` AS "parrocchia", 
+    `parrocchie`.`id` AS "id_parrocchia",
+    s.`id` AS "id_staffista", 
+    IFNULL(p.`is_referente`, FALSE) AS "referente", 
+    p.`maglia`,
+    IFNULL(GROUP_CONCAT(DISTINCT c.`nome` SEPARATOR ', '), '') AS "lista_commissioni",
+
+    e.`anno`,
+    e.`id` AS "id_edizione"
+FROM `staffisti` s
+    INNER JOIN `anagrafiche_espanse` a ON s.`dati_anagrafici` = a.`id`
+    INNER JOIN `parrocchie` ON s.`parrocchia` = `parrocchie`.`id`
+    CROSS JOIN `edizioni` e
+    LEFT OUTER JOIN `partecipaz_staff_ediz` p ON p.`staff` = s.`id` AND p.`edizione` = e.`id`
+    LEFT OUTER JOIN `ruoli_staff` r ON s.`id` = r.`staffista` AND r.`edizione` = e.`id`
+    LEFT OUTER JOIN `commissioni` c ON r.`commissione` = c.`id`
+WHERE e.`anno` = YEAR(CURRENT_DATE)
+GROUP BY s.`id`
+ORDER BY `parrocchie`.`nome`, a.`cognome`, a.`nome`;
