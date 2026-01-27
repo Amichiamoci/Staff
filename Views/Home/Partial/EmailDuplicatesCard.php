decef49fce85b4e3f59c40f6297f83cc244d1185
Richie314 <richieinleghorn@gmail.com>
2026-01-27 10:18:16 +0100

    Convert nested list to details/summary

--- Views/Home/Partial/EmailDuplicatesCard.php
+++ Views/Home/Partial/EmailDuplicatesCard.php
@@ -11,28 +11,33 @@
     function email_duplicates_js_decoder(i) {
         const li = document.createElement('li');
         li.className = 'list-group-item';
-        if (i.total > 2)
-            li.classList.add('text-danger');
-        else
-            li.classList.add('text-warning');
-        
-        li.innerText = `${i.email} (${i.total}):`;
 
-        const inner_ul = document.createElement('ul');
-        inner_ul.className = 'list-group-flush d-inline-block ms-3';
-        li.appendChild(inner_ul);
+        const details = document.createElement('details');
+        li.appendChild(details);
+
+        const summary = document.createElement('summary');
+        summary.innerText = `${i.email} (${i.total})`;
+        if (i.total > 2)
+            summary.classList.add('text-danger');
+        else
+            summary.classList.add('text-warning');
+        details.appendChild(summary);
+
+        const ul = document.createElement('ul');
+        ul.className = 'list-group-flush d-inline-block ms-1';
+        details.appendChild(ul);
 
         for (const a of i.who)
         {
             const inner_li = document.createElement('li');
-            inner_li.className = 'list-group-item p-0 d-inline';
+            inner_li.className = 'list-group-item p-0';
+            ul.appendChild(inner_li);
 
             const a_el = document.createElement('a');
             a_el.href = `<?= $P ?>/staff/edit_anagrafica?id=${a.id}`;
             a_el.innerText = a.name;
-            a_el.className = 'link-underline-opacity-0 text-reset me-1';
+            a_el.className = 'link-underline-opacity-0 link-secondary p-0 m-0';
             inner_li.appendChild(a_el);
-            inner_ul.appendChild(inner_li);
         }
 
         return li;
